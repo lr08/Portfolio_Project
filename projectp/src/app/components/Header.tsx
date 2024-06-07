ade549859124f43e037b742e6500b7ac67ca6868
Lakshay Rawal <lakshayrawal08@gmail.com>
2024-06-07 16:00:20 +0530

    mobile view supported

--- projectp/src/app/components/Header.tsx
+++ projectp/src/app/components/Header.tsx
@@ -1,37 +1,71 @@
-import React from "react";
+'use client';
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from 'next/image';
 const Header = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+  
   return (
     <header>
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <a href="https://flowbite.com" className="flex items-center">
+    <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+        <a href="https://portfolio-project-woad-eight.vercel.app/" className="flex items-center">
           <Image
-              src="/lr.svg"
-              className="mr-3 h-6 sm:h-9"
-              alt="LR"
-              width={36} // Specify the width for the Image component
-              height={36} // Specify the height for the Image component
-            />
-           
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Lakshay Rawal
-            </span>
-          </a>
-          <div className="flex items-center lg:order-2">
-            <a
-              href="#"
-              className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
-            >
-              Admin Log in
-            </a>
-          </div>
-          <div
-            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-            id="mobile-menu-2"
+            src="/lr.svg"
+            className="mr-3 h-6 sm:h-9"
+            alt="LR"
+            width={36}
+            height={36}
+          />
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            Lakshay Rawal
+          </span>
+        </a>
+        <div className="flex items-center lg:order-2">
+          <a
+            href="#"
+            className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
           >
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            Admin Log in
+          </a>
+          <button
+            type="button"
+            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="mobile-menu-2"
+            aria-expanded={menuOpen}
+            onClick={toggleMenu}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d={menuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`${
+            menuOpen ? "block" : "hidden"
+          } justify-between items-center w-full lg:flex lg:w-auto lg:order-1`}
+          id="mobile-menu-2"
+        >
+          <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <li>
               <Link
                 href="/Home"
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
@@ -39,29 +73,36 @@ const Header = () => {
               >
                 Home
               </Link>
+            </li>
+            <li>
               <Link
                 href="/Projects"
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Projects
               </Link>
+            </li>
+            <li>
               <Link
-                href="OnlinePresence"
+                href="/OnlinePresence"
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Online Presence
               </Link>
+            </li>
+            <li>
               <Link
-                href="ContactUs"
+                href="/ContactUs"
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Contact Us
               </Link>
-            </ul>
-          </div>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </header>
+      </div>
+    </nav>
+  </header>
   );
 };
 
